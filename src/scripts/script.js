94506ba40ebf933d76a8437242029385fcb76d5d
Mikhail Golaykov <68639034+MichaelG27@users.noreply.github.com>
2021-04-26 16:43:38 +0400

    Delete src directory

--- src/scripts/script.js
+++
@@ -1,9 +0,0 @@
-$('#menu').on("click", function () {
-  $('.hamburger').slideToggle(300, function(){
-    if ($(this).css('display') === 'none') {
-      $('.hamburger').removeAttr('style')
-    }
-  })
-})
-// $('.hamburger').css('height', document.body.scrollHeight);
-console.log(window.innerHeight)
