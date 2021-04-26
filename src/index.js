63be05338ef66b36dd2396a64c0d7c5c6963c74b
Mikhail Golaykov <68639034+MichaelG27@users.noreply.github.com>
2021-04-26 16:34:29 +0400

    Car shop
    It is part of my portfolio. Created to show my professional skills. Isn`t commercial site.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './index.html'
+import './cars.html'
+import './about.html'
+import './scripts/script'
+import './styles/about.css'
+import './styles/cars.css'
+import './styles/default.css'
+import './styles/index.css'
+
+
